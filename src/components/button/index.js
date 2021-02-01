13bf3d2c5e5ab9d5d4319b9599dc88005e5e4451
Wisnu Pratama <wisnuputrapratama24@gmail.com>
2021-02-01 12:00:48 +0700

    update

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -10,10 +10,10 @@ Button.Logo = function ButtonLogo({ logo, text, link }) {
       href={link}
       rel='noopenner noreferrer'
       target='_blank'
-      className='button-tailwind inline-flex bg-white focus:outline-none dark:text-gray-200 text-xss lg:text-sm font-medium md:px-2 md:py-2 md:m-4 rounded-full'>
+      className='button-tailwind inline-flex bg-white focus:outline-none dark:text-gray-200 text-xs lg:text-sm font-medium px-1 py-1 md:px-2 md:py-2 md:m-4 rounded-full'>
       <div className='flex items-center md:px-2 p-1'>
         <img src={logo} alt='logo tailwindcss' className='md:h-4 h-2 md:mr-2' />{" "}
-        <p className='text-xss md:text-base'>{text}</p>
+        <p className='text-xs md:text-base'>{text}</p>
       </div>
     </a>
   );
